8a05f64c966e2aaaa4eeeafd6c62c2c7df319812
Isaac Mendez <isaacmendez0824@gmail.com>
2026-05-05 01:42:35 -0700

    added script code
    finished adding scripts in

--- scripts/gameplay/PromptList.gd
+++ scripts/gameplay/PromptList.gd
@@ -1,11 +1,21 @@
 extends Node
 
+var words: Array[String] = [
+	"apple", "banana", "cherry", "dragon", "elephant",
+	"forest", "galaxy", "horizon", "island", "jungle",
+	"kitten", "lion", "mountain", "nebula", "ocean",
+	"planet", "quartz", "rocket", "sunrise", "tiger",
+	"umbrella", "volcano", "whisper", "xylophone", "yellow",
+	"zebra", "anchor", "bridge", "canyon", "desert",
+	"energy", "feather", "guitar", "hammer", "iceberg",
+	"jacket", "kangaroo", "lantern", "mirror", "notebook",
+	"orange", "puzzle", "quokka", "rainbow", "satellite",
+	"tornado", "universe", "village", "window", "yogurt",
+	"zephyr", "breeze", "castle", "diamond", "ember",
+	"flame", "glacier", "harbor", "insect", "journey"
+]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_prompt() -> String:
+	var word_index = randi() % words.size()
+	var word = words[word_index]
+	return word
