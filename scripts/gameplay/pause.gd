e832719c746201640094086291083f228db35601
Jsalazar68 <jsalazar68@csub.edu>
2026-05-05 21:22:06 -0700

    Added a pause node
    Pause node contains the pausing logic

---
+++ scripts/gameplay/pause.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func pause():
+	get_tree().paused = true
+
+func resume():
+	get_tree().paused = false
+
+func _input(event):
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+		print("escape pressed for pausing")
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
+		print("escape pressed for resuming")
