2838aea7fc122f8bac13bcbbead26aa5e35a4dde
Isaac Mendez <isaacmendez0824@gmail.com>
2026-05-06 02:11:20 -0700

    had to get rid of old enemy
    the enemy scene and gd corrupted

--- scripts/gameplay/Network.gd
+++ scripts/gameplay/Network.gd
@@ -48,6 +48,5 @@ func _on_request_completed(result, response_code, headers, body):
 	var leaderboard = JSON.parse_string(text)
 	print("Server response:", text)
 	print("Leaderboard: ", leaderboard)
-
 	leaderboard_data = leaderboard
 	
